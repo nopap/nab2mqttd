5ca2c0429263a557428305523baaf232bd5c0bf1
Jean Fridisch <jfridisch@fortinet-us.com>
2020-11-26 12:40:45 +0100

    Fix async issues as exposed in https://github.com/nopap/nab2mqttd/issues/1 Change PIDFile in .service Add DAEMON_PIDFILE in Nab2MQTTd Clean some indentation

--- nab2mqttd.py
+++ nab2mqttd.py
@@ -8,6 +8,7 @@ import json
 import paho.mqtt.client as mqtt
 
 class Nab2MQTTd(NabInfoService):
+    DAEMON_PIDFILE = "/run/nab2mqttd.pid"
 
     # on_connect callback
     def on_connect(self, client, userdata, flags, rc):
@@ -25,38 +26,38 @@ class Nab2MQTTd(NabInfoService):
     # {"type":"message", "body":[{"audio":["nabsurprised/2.mp3"]}]}
     # {"type":"message", "body":[{"audio":["nabsurprised/2.mp3"]}], "expiration":"TAGEXPIRATION"}
     def on_message(self, client, userdata, msg):
-          logging.debug(msg.payload)
-          packet = str(msg.payload.decode("utf-8","ignore"))
-          #state = json.loads(packet)
-          #logging.debug(state)
+        logging.debug(msg.payload)
+        packet = str(msg.payload.decode("utf-8","ignore"))
+        #state = json.loads(packet)
+        #logging.debug(state)
 
-          # playing animation via the self.perform method; allows to have the animation properly handled by the NabService
-          if '"type":"info"' in packet:
+        # playing animation via the self.perform method; allows to have the animation properly handled by the NabService
+        if '"type":"info"' in packet:
             #logging.debug("info is in da place")
             state = json.loads(packet)
             dump = json.dumps(state["animation"])
             #logging.debug("dump: " + dump)
             self.infopacket = dump
+            next_date, next_args, config_t = async_to_sync(self.get_config)()
             now = datetime.datetime.now(datetime.timezone.utc)
             expiration = now + datetime.timedelta(minutes=1)
-            async_to_sync(self.perform)(expiration, "info", self.get_config())
+            async_to_sync(self.perform)(expiration, "info", config_t)
             return
 
-          # replace TAGEXPIRATION string by properly formatted expiration datetime
-          if "TAGEXPIRATION" in packet:
+        # replace TAGEXPIRATION string by properly formatted expiration datetime
+        if "TAGEXPIRATION" in packet:
             now = datetime.datetime.now(datetime.timezone.utc)
             expiration = now + datetime.timedelta(minutes=1)
             packet = packet.replace("TAGEXPIRATION", expiration.isoformat())
 
-          # add mandatory carriage return
-          packet = packet + "\r\n"
-          self.writer.write(packet.encode("utf8"))
-          async_to_sync(self.writer.drain)()
-            
+        # add mandatory carriage return
+        packet = packet + "\r\n"
+        self.writer.write(packet.encode("utf8"))
+        async_to_sync(self.writer.drain)()
+
     def __init__(self):
         super().__init__()
         from . import models
-
         config = models.Config.load()
         logging.debug("Config.server: " + str(config.server))
         #logging.debug("Config.clientid: " + str(config.clientid))
@@ -66,25 +67,22 @@ class Nab2MQTTd(NabInfoService):
         #logging.debug("Config.tls: " + str(config.tls))
         #logging.debug("Config.tlsinsecure: " + str(config.tlsinsecure))
         logging.debug("Config.topic: " + str(config.topic))
-
         self.infopacket = None
-
         self.client = mqtt.Client(client_id = config.clientid)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         if config.username:
           self.client.username_pw_set(config.username, config.password)
         if config.tls == 'true':
-          self.client.tls_set("/home/pi/pynab/nab2mqttd/ca.crt") # merdique... 
+          self.client.tls_set("/home/pi/pynab/nab2mqttd/ca.crt") # merdique...
         if config.tlsinsecure == 'true':
           self.client.tls_insecure_set(True)
         self.client.connect(config.server, int(config.port), 60)
         self.client.loop_start()
 
-    def get_config(self):
+    async def get_config(self):
         from . import models
-        
-        config = models.Config.load()
+        config = await models.Config.load_async()
         return (
             config.next_performance_date,
             config.next_performance_type,
@@ -100,13 +98,13 @@ class Nab2MQTTd(NabInfoService):
             ),
         )
 
-    def update_next(self, next_date, next_args):
+    async def update_next(self, next_date, next_args):
         #logging.debug("update_next")
         from . import models
-        config = models.Config.load()
+        config = await models.Config.load_async()
         config.next_performance_date = next_date
         config.next_performance_type = next_args
-        config.save()
+        await config.save_async()
 
     async def fetch_info_data(self, config_t):
         #logging.debug("fetch_info_data")
